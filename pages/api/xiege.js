6bfda90cb56b9784092cccecc9a304af27ad75f5
左康生 <zhen0578@qq.com>
2023-03-22 11:47:46 +0800

    initial

--- pages/api/xiege.js
+++ pages/api/xiege.js
@@ -1,20 +1,20 @@
-
 export default async function (req, res) {
   res.writeHead(200, {
     // 'Content-Type': 'text/event-stream',
     Connection: 'close',
+    'Transfer-Encoding': 'chunked',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
   });
   res.write(`id: ${new Date().getTime()}\n\n`);
-  res.write(`data: ${JSON.stringify({ message: 'First1 message' })}\n\n`);
+  res.write(`data: ${JSON.stringify({ message: 'First11 message' })}\n\n`);
   setTimeout(() => {
     res.write(`id: ${new Date().getTime()}\n\n`);
-    res.write(`data: ${JSON.stringify({ message: 'Second2 message' })}\n\n`);
+    res.write(`data: ${JSON.stringify({ message: 'Second22 message' })}\n\n`);
   }, 1000);
   setTimeout(() => {
     res.write(`id: ${new Date().getTime()}\n\n`);
-    res.write(`data: ${JSON.stringify({ message: 'Third3 message' })}\n\n`);
+    res.write(`data: ${JSON.stringify({ message: 'Third33 message' })}\n\n`);
     res.end();
   }, 2000);
 }

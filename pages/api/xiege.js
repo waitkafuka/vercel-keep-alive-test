98458bf2eb15d00ff2b55a14b7e71b325f9be69e
左康生 <zhen0578@qq.com>
2023-03-22 14:24:47 +0800

    initial

--- pages/api/xiege.js
+++ pages/api/xiege.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 export default async function (req, res) {
   res.writeHead(200, {
     // 'Content-Type': 'text/event-stream',
@@ -6,15 +7,7 @@ export default async function (req, res) {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type',
   });
-  res.write(`id: ${new Date().getTime()}\n\n`);
-  res.write(`data: ${JSON.stringify({ message: 'First11 message' })}\n\n`);
-  setTimeout(() => {
-    res.write(`id: ${new Date().getTime()}\n\n`);
-    res.write(`data: ${JSON.stringify({ message: 'Second22 message' })}\n\n`);
-  }, 1000);
-  setTimeout(() => {
-    res.write(`id: ${new Date().getTime()}\n\n`);
-    res.write(`data: ${JSON.stringify({ message: 'Third33 message' })}\n\n`);
-    res.end();
-  }, 2000);
+
+  const readStream = fs.createReadStream(__dirname+'/data.txt');
+  readStream.pipe(res);
 }
